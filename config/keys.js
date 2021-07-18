620c4361b9de3e080e794377699cfd1c0b2305ac
Sujeet Singh <sujeets207@gmail.com>
2021-07-18 14:39:02 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb://soluge_admin:solugepassword@cluster0-shard-00-00.qjbrr.mongodb.net:27017,cluster0-shard-00-01.qjbrr.mongodb.net:27017,cluster0-shard-00-02.qjbrr.mongodb.net:27017/solution?ssl=true&replicaSet=atlas-mm41px-shard-0&authSource=admin&retryWrites=true&w=majority'
+dbPassword = ''
 
 module.exports = {
     mongoURI: dbPassword
